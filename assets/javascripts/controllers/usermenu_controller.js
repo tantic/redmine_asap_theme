ccda3233fb8680daa7fd030b6fc405c59b93dd29
Tantic <mailotantic@gmail.com>
2024-01-08 22:45:06 +0100

    add search to navbar
    Signed-off-by: Tantic <mailotantic@gmail.com>

--- assets/javascripts/controllers/usermenu_controller.js
+++ assets/javascripts/controllers/usermenu_controller.js
@@ -4,13 +4,10 @@
   application.register("usermenu", class extends Stimulus.Controller {
 
     static get targets() {
-      return [ "usersubmenu" ]
+      return [ "usersubmenu", "search", "searchinput" ]
     }
 
-    connect() {
-      console.log("user menu test ")
-      console.log("Submenu "+this.usersubmenuTarget)
-    }
+    connect() {}
 
     toggle(event){
       this.usersubmenuTarget.classList.toggle('hidden');
@@ -19,17 +16,27 @@
 
     dismissOnClick(element) {
       document.addEventListener('click', (evt) => {
-          var isClickInside = element.contains(evt.target);
-          if (!isClickInside) {
-              console.log("clic en dehors")
-              element.classList.add('hidden')
-          }
+        var isClickInside = element.contains(evt.target);
+        if (!isClickInside) {
+          element.classList.add('hidden')
+        }
       },
       { once: true, capture: true }
-    );
-  }
+     );
+    }
 
-
+    search(event) {
+      event.preventDefault();
+      this.searchTargets.forEach((search) => {
+        if (search.classList.contains("hidden")) {
+          search.classList.remove("hidden");
+          this.searchinputTarget.focus();
+          this.dismissOnClick(this.searchTarget);
+        } else {
+          search.classList.add("hidden");
+        }
+      });
+    }
 
   })
 
