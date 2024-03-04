c81447ca2d067a1d2a3dfcbbd0d1ea17ffebeaf6
Tantic <mailotantic@gmail.com>
2024-03-04 23:23:20 +0100

    improve projects page
    Signed-off-by: Tantic <mailotantic@gmail.com>

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
   ]
 }
 
