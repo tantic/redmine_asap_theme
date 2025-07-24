a375f44bac243c5ce2874db155e5ddbf5cf66ef9
Tantic <mailotantic@gmail.com>
2025-07-24 19:45:28 +0200

    add compatibility with redmine_asap_pilot
    Signed-off-by: Tantic <mailotantic@gmail.com>

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_asap_theme do
   name 'Redmine Asap Theme plugin'
-  author 'Tantic'
+  author 'DGAC/DSNA - Tantic'
   description 'UX/UI based on Tailwindcss'
   version '1.0.0'
   url 'https://github.com/tantic/redmine_asap_theme'
